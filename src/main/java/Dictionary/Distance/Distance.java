83c5989601703b88505f5fee99ffc60ea77b5dcc
DoguhanB <73018150+DoguhanB@users.noreply.github.com>
2022-05-12 01:57:49 +0300

    Add files via upload

---
+++ src/main/java/Dictionary/Distance/Distance.java
@@ -0,0 +1,28 @@
+package Dictionary.Distance;
+
+public interface Distance {
+
+    static int ldist(CharSequence str1, CharSequence str2) {
+
+        int result;
+
+        if (str1.length() == 0) {
+            result = str2.length();
+        } else if (str2.length() == 0) {
+            result = str1.length();
+        } else {
+            int distSub = ldist(str1.subSequence(0, str1.length() - 1), str2.subSequence(0, str2.length() - 1));
+            if (str1.charAt(str1.length() - 1) == str2.charAt(str2.length() - 1)) {
+                result = distSub;
+            } else {
+                int distDel = ldist(str1.subSequence(0, str1.length() - 1), str2);
+                int distIns = ldist(str1, str2.subSequence(0, str2.length() - 1));
+                result = 1 + (Math.min(Math.min(distSub, distDel), distIns));
+            }
+        }
+
+        return result;
+    }
+
+    //other
+}
