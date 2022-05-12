b15f35514273cabdca6b3897499d4d26d681ecf5
DoguhanB <73018150+DoguhanB@users.noreply.github.com>
2022-05-12 10:44:33 +0300

    Update Distance.java

--- src/main/java/Dictionary/Distance/Distance.java
+++ src/main/java/Dictionary/Distance/Distance.java
@@ -2,7 +2,7 @@ package Dictionary.Distance;
 
 public interface Distance {
 
-    static int ldist(CharSequence str1, CharSequence str2) {
+    static int ldist(String str1, String str2) {
 
         int result;
 
@@ -11,12 +11,12 @@ public interface Distance {
         } else if (str2.length() == 0) {
             result = str1.length();
         } else {
-            int distSub = ldist(str1.subSequence(0, str1.length() - 1), str2.subSequence(0, str2.length() - 1));
+            int distSub = ldist(str1.substring(0, str1.length() - 1), str2.substring(0, str2.length() - 1));
             if (str1.charAt(str1.length() - 1) == str2.charAt(str2.length() - 1)) {
                 result = distSub;
             } else {
-                int distDel = ldist(str1.subSequence(0, str1.length() - 1), str2);
-                int distIns = ldist(str1, str2.subSequence(0, str2.length() - 1));
+                int distDel = ldist(str1.substring(0, str1.length() - 1), str2);
+                int distIns = ldist(str1, str2.substring(0, str2.length() - 1));
                 result = 1 + (Math.min(Math.min(distSub, distDel), distIns));
             }
         }
